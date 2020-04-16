b1a87bea3c400e5e4d835c805e750d9a8cf89769
balaji38738 <bijjapwar123@gmail.com>
2020-04-16 13:19:00 +0530

    REFACTOR: Code optimization

--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -1,10 +1,17 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
     @Test
     public void whenGivenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -13,7 +20,6 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
@@ -22,7 +28,6 @@ public class InvoiceGeneratorTest {
 
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare() {
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] rides = {
                 new Ride(2.0, 5),
                 new Ride(0.1, 1)
