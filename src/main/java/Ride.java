b232f1eb990621a769c31fa3208df236c30d7e2c
balaji38738 <bijjapwar123@gmail.com>
2020-04-17 16:29:38 +0530

    ADDED: Code for premium and normal rides

--- src/main/java/Ride.java
+++ src/main/java/Ride.java
@@ -1,9 +1,12 @@
 public class Ride {
     public int time;
     public double distance;
+    public enum RideType {NORMAL, PREMIUM};
+    public RideType rideType;
 
-    public Ride(double distance, int time) {
+    public Ride(double distance, int time, RideType rideType) {
         this.distance = distance;
         this.time = time;
+        this.rideType = rideType;
     }
 }
